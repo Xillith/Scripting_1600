6c261ab590306b9ddad024a82c3aed85cd793a3d
Ben <sixjedi@gmail.com>
2016-01-21 17:32:41 -0700

    no message

---
+++ 1600 Labs/Assets/Lab1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Lab1 : MonoBehaviour {
+
+    void Start()
+    {
+        int j = 37;
+        int k;
+        double p= 223.2;
+
+        j=ThisIsAfunction(j);
+        Debug.Log(j);
+        print("Hello "+j);
+
+    }
+
+  private int ThisIsAfunction(int i) {
+        char x = 'i';
+        return i+1;
+    }
+}
---
+++ 1600 Labs/Assets/Lab2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lab2 : MonoBehaviour {
+    // Use this for initialization
+ 
+
+    void Start()
+    {
+        //int j;
+
+        int whatsthis = RandomRolling(5);
+        print(" Hmm " + whatsthis);
+
+
+    }
+
+    static int RandomRolling(int How_Many_D6)
+    {
+
+        print("I do nothing with this! " + How_Many_D6);
+
+        return 42;
+    }
+
+
+}
