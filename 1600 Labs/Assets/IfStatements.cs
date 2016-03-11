ee84f6e0444ddb7f8f509c6ebbd886e43e72ff15
Ben <sixjedi@gmail.com>
2016-03-10 17:36:57 -0700

    no message

---
+++ 1600 Labs/Assets/IfStatements.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int i = 4;
+
+        if (i == 4) { print("4"); }
+        else if (i == 4) { print("3"); }
+        else { print("Failure"); }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
