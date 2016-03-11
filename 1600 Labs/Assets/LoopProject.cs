1f4b983c97dd8dc9920beee7c535c290b1516bc0
Ben <sixjedi@gmail.com>
2016-03-10 17:49:49 -0700

    no message

---
+++ 1600 Labs/Assets/LoopProject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopProject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] strings = new string[3];
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+        int j = 0;
+
+        foreach (string Temp in strings) {
+            print(Temp);
+        }
+        for (int i = 5; i > 0; i--) {
+            print("HI");
+        }
+        
+        while (j < 10) {
+            j++;
+            print(j);
+        }
+        int k = 1;
+        do { print("k: " + k);
+            k++;
+        }
+        while (k < 10);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
