2b4af27c95f57c8a9997c9d8c5f1c0ac1d107f7d
Ben <sixjedi@gmail.com>
2016-03-24 18:11:01 -0600

    no message

---
+++ 1600 Labs/Assets/ClassPages.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassPages : MonoBehaviour {
+
+    public class ThisIsAClass {
+
+        public int hi;
+        public int low;
+        public ThisIsAClass() {
+            hi = 10;
+            low = 5; }
+        public ThisIsAClass(int H, int L)
+        {
+            hi = H;
+            low = L;
+        }
+
+
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        ThisIsAClass ThisClass = new ThisIsAClass(45, 2);
+
+        ThisIsAClass cls2 = ThisClass;
+
+        cls2.hi = 15;
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+  
+}
