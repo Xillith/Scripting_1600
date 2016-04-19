1374df956455a2e52ea59e2dbe59be06a84c4af9
Ben <sixjedi@gmail.com>
2016-04-19 17:09:51 -0600

    no message

---
+++ 1600 Labs/Assets/Arrays.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+    public int[] Hppy = { 3,4,5,6};
+
+
+	// Use this for initialization
+	void Start () {
+        Hppy[0] = 34;
+
+        for (int i = 0; i<Hppy.Length; i++) {
+            Hppy[i] = i + 33;
+            print(Hppy[i]);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+}
