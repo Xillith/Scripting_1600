ed5a0fe3babe56598ca4623d6e03ca98b4e9df13
Ben <sixjedi@gmail.com>
2016-04-19 17:48:02 -0600

    no message

---
+++ 1600 Labs/Assets/Switch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+ public   int Temperature = 300;
+
+	// Use this for initialization
+	void Start () {
+
+        switch (Temperature){
+            case 300:
+                    print("Im At 300");
+                break;
+            case 234:
+                print("Im At 234");
+                break;
+            default:
+                print("Frozen");
+                break;
+
+
+
+
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
