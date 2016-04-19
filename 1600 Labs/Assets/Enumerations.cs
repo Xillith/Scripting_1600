4f566b6bd7e2a2f1d28b7486d8bb25cb6b8ca0fc
Ben <sixjedi@gmail.com>
2016-04-19 17:35:05 -0600

    no message

---
+++ 1600 Labs/Assets/Enumerations.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehaviour {
+ 
+    enum Directions  {North, South, East, Dude};
+
+	// Use this for initialization
+	void Start () {
+        Directions thisIsAVar = Directions.North;
+        Debug.Log(thisIsAVar);
+        thisIsAVar =aFunctrion(thisIsAVar);
+
+        Debug.Log(thisIsAVar);
+
+
+    }
+
+    Directions aFunctrion(Directions grr) {
+
+        if (grr == Directions.North)
+            return Directions.South;
+        else return Directions.East;
+
+    }
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
